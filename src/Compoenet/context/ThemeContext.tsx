3c530fae9a222c2ac6620b46715c6891327d16a5
Vaibhav-1529 <vaibhavsaini77786@gmail.com>
2025-08-17 16:01:59 +0530

    update

--- src/Compoenet/context/ThemeContext.tsx
+++ src/Compoenet/context/ThemeContext.tsx
@@ -2,7 +2,7 @@
 import { Theme } from "@radix-ui/themes";
 import { createContext, useState } from "react";
 
-export const Context=createContext();
+export const Context=createContext({});
 export default function ThemeContext({
   children,
 }: Readonly<{
