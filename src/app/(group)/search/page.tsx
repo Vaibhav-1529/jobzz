7b5af5391d7389c193347d38f69d1040b219e895
Vaibhav-1529 <vaibhavsaini77786@gmail.com>
2025-08-17 18:55:56 +0530

    path correct

--- src/app/(group)/search/page.tsx
+++ src/app/(group)/search/page.tsx
@@ -3,20 +3,28 @@
 import JobCard from "@/components/cards/job-card";
 import CardLoading from "@/components/lodingstate/CardLoading";
 import { useSearchParams } from "next/navigation";
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useState,Suspense } from "react";
 import { UserContext } from "../layout";
 import Loading from "@/components/lodingstate/Loading";
 
 export default function SearchPage() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <SearchPageContent />
+    </Suspense>
+  );
+}
+
+function SearchPageContent() {
   const searchparams = useSearchParams();
-  const {user}=useContext(UserContext)
+  const { user } = useContext(UserContext);
   const q = searchparams.get("q") || "";
   const et = searchparams.get("et") || "";
-  const jt=searchparams.get("jt")||"";
+  const jt = searchparams.get("jt") || "";
   const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   const [isLoading, setIsLoading] = useState(true);
   const [jobs, setJobs] = useState([]);
-  if(!user) return <Loading/>;
+  if (!user) return <Loading />;
   useEffect(() => {
     async function fetchJobs() {
       try {
@@ -33,20 +41,19 @@ export default function SearchPage() {
       }
     }
     fetchJobs();
-  }, [q, et,jt]);
+  }, [q, et, jt]);
 
   return (
     <main className="max-w-7xl m-auto h-screen">
-  <div className="h-full overflow-y-auto py-5 px-4 scrollbar-hidden pb-20">
-    <div className="grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-6">
-      {isLoading
-        ? arr.map((item) => <CardLoading key={item} fromSearch />)
-        : jobs.map((job) => (
-            <JobCard fromSearch={true} key={job.id} job={job} />
-          ))}
-    </div>
-  </div>
-</main>
-
+      <div className="h-full overflow-y-auto py-5 px-4 scrollbar-hidden pb-20">
+        <div className="grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-6">
+          {isLoading
+            ? arr.map((item) => <CardLoading key={item} fromSearch />)
+            : jobs.map((job) => (
+                <JobCard fromSearch={true} key={job.id} job={job} />
+              ))}
+        </div>
+      </div>
+    </main>
   );
 }
